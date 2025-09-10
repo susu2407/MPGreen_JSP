b1dda1f1fc93d4242900617e436082d5253f0975
susu <tndus159200297@gmail.com>
2025-09-10 17:42:19 +0900

    0910tndusㅣWIP:login...

--- MPGreen/src/main/java/greendae/user/controller/member/login/LoginController.java
+++ MPGreen/src/main/java/greendae/user/controller/member/login/LoginController.java
@@ -17,7 +17,7 @@ public class LoginController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/longin.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/login.jsp");
 		dispatcher.forward(req, resp);
 		
 	}
