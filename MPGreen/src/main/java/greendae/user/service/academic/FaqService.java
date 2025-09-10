e41930e30403dfb975fc8a41af0c35fdd59d9c2a
o-seoj <tjwjd010@naver.com>
2025-09-10 12:35:56 +0900

    o-seoj | faq dao, dto, service

---
+++ MPGreen/src/main/java/greendae/user/service/academic/FaqService.java
@@ -0,0 +1,28 @@
+package greendae.user.service.academic;
+
+import java.util.List;
+
+import greendae.user.dao.academic.FaqDAO;
+import greendae.user.dto.academic.FaqDTO;
+
+public enum FaqService {
+	INSTANCE;
+	
+	private FaqDAO dao = FaqDAO.getInstance();
+	
+	public void register(FaqDTO dto) {
+		dao.insert(dto);
+	}	
+	public FaqDTO findById(int cate, int num) {
+		return dao.select(cate, num);
+	}
+	public List<FaqDTO> findAll() {
+		return dao.selectAll();
+	}
+	public void modify(FaqDTO dto) {
+		dao.update(dto);
+	}	
+	public void remove(int cate, int num) {
+		dao.delete(cate, num);
+	}
+}
